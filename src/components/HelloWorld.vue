63a8d2641b8d4a2e8074ec44dcf9dbd90da61ee4
unknown <1909590616@qq.com>
2018-12-20 17:52:14 +0800

    vuevuexaxios

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  
+
+created() {
+    this.$store.dispatch('getMineBaseApi');
+  },
+
+ computed: {
+    ...mapGetters([
+      'getMineBaseMsg'
+    ])
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
